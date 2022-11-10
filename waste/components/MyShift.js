67d227bc00d9e2248a1b0b7473d0c7e1ff495ee1
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2022-11-10 18:44:10 +0530

    dd

---
+++ waste/components/MyShift.js
@@ -0,0 +1,30 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function MyShift() {
+  const ListEmptyComponent = () => {
+    return (
+      <View>
+        <Message info message="Null for now" />
+      </View>
+    );
+  };
+
+  const renderItem = ({ item }) => {};
+  return (
+    <TouchableOpacity>
+      <Text>no 1</Text>
+    </TouchableOpacity>
+  );
+}
+return (
+  <View>
+    <FlatList
+      renderItem={renderItem}
+      data={[data]}
+      KeyExtractor={(item) => String(item.id)}
+      ListEmptyComponent={ListEmptyComponent}
+    />
+  </View>
+);
