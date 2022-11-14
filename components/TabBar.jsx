7576be99b20dfdaf9c31a674c1c1138bab41f2cc
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2022-11-14 14:14:48 +0530

    tab

---
+++ components/TabBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TabBar() {
+  return (
+    <View>
+      <View>
+        <Text>Helnsiki</Text>
+      </View>
+      <View>
+        <Text>Tempere</Text>
+      </View>
+      <View>
+        <Text>Turku</Text>
+      </View>
+    </View>
+  );
+}
--- screeno/Available.jsx
+++ screeno/Available.jsx
@@ -1,35 +1,36 @@
-import { View, Text,StyleSheet} from 'react-native'
-import React from 'react'
-import ButtonBox from '../components/ButtonBox'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import ButtonBox from "../components/ButtonBox";
+import TabBar from "../components/TabBar";
 
-
-export default function Available({navigation}) {
+export default function Available({ navigation }) {
   return (
-    <View style= {{flex:1}} >
-      <Text>available</Text>
+    <View style={{ flex: 1 }}>
+      <View>
+        <TabBar />
+      </View>
 
-
-  <View style = {styles.row}>
-  <ButtonBox title="My shifts" onPress={()=> navigation.navigate('Available')}/> 
-  <ButtonBox title="Available Shifts" onPress={()=> navigation.navigate('Shift')}/> 
-
-
-  </View>
-
-
-
+      <View style={styles.row}>
+        <ButtonBox
+          title="My shifts"
+          onPress={() => navigation.navigate("Available")}
+        />
+        <ButtonBox
+          title="Available Shifts"
+          onPress={() => navigation.navigate("Shift")}
+        />
+      </View>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  row:{
-flexDirection: "row",
-alignItems : "center",
-alignSelf : "center",
-      position: "absolute",
-      width:"100%",
-      bottom : 80,
-      justifyContent : "center"
-
-  }
-})
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    position: "absolute",
+    width: "100%",
+    bottom: 80,
+    justifyContent: "center",
+  },
+});
