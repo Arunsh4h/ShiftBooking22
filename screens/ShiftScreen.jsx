ab95935ba6aa99816ad475ce01587c7053efb6c8
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2022-11-14 14:15:08 +0530

    Tab

--- screens/ShiftScreen.jsx
+++ screens/ShiftScreen.jsx
@@ -1,13 +1,19 @@
-import {TouchableOpacity, StyleSheet, Text, View, StatusBar, SafeAreaView} from 'react-native';
-import React, {Fragment, useEffect, useState} from 'react';
-import Heading from '../components/Heading';
-import Shift from '../components/Shift';
-import axios from 'axios';
-import { Colors } from "../constants/Colors"
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import React, { Fragment, useEffect, useState } from "react";
+import Heading from "../components/Heading";
+import Shift from "../components/Shift";
+import axios from "axios";
+import { Colors } from "../constants/Colors";
 
-
-import { ScrollView } from 'react-native-gesture-handler';
-const url = 'http://127.0.0.1:8080/shifts';
+import { ScrollView } from "react-native-gesture-handler";
+const url = "http://127.0.0.1:8080/shifts";
 // const url = "https://reactnative.dev/movies.json"
 
 const ShiftScreen = () => {
@@ -22,29 +28,23 @@ const ShiftScreen = () => {
   }, []);
 
   return (
-    
     <SafeAreaView style={styles.backgroundStyle}>
-         <StatusBar />
-    <ScrollView>
-    <View>
-      <Heading title={'Today'} noOfShift={'2 shifts'} hrs={'4 h'} />
-      
-      {shifts?.map((e, i) => (
-        <Fragment key={i}>
-          <Shift desc={e.area} from={e.startTime} to={e.endTime} />
-        </Fragment>
+      <StatusBar />
+      <ScrollView>
+        <View>
+          <Heading title={"Today"} noOfShift={"2 shifts"} hrs={"4 h"} />
 
-      ))}
-<Heading title={'Tomorrow'} noOfShift={'2 shifts'} hrs={'4 h'} />
-<Heading title={'Tomorrow'} noOfShift={'2 shifts'} hrs={'4 h'} />
-
-    </View>
-    <View style={styles.footerContainer}>
-         
+          {shifts?.map((e, i) => (
+            <Fragment key={i}>
+              <Shift desc={e.area} from={e.startTime} to={e.endTime} />
+            </Fragment>
+          ))}
+          <Heading title={"Tomorrow"} noOfShift={"2 shifts"} hrs={"4 h"} />
+          <Heading title={"Tomorrow"} noOfShift={"2 shifts"} hrs={"4 h"} />
         </View>
-    </ScrollView>
+        <View style={styles.footerContainer}></View>
+      </ScrollView>
     </SafeAreaView>
-
   );
 };
 
@@ -59,17 +59,14 @@ const styles = StyleSheet.create({
     height: 65,
     alignItems: "center",
     justifyContent: "center",
-    display : 'flex',
-    textAlign : 'center',
-  
+    display: "flex",
+    textAlign: "center",
   },
-  footerContainer : {
-    display :'flex',
-    flexDirection : 'row',
-  
-   
+  footerContainer: {
+    display: "flex",
+    flexDirection: "row",
   },
-  backgroundStyle : {
-    flex : 1,
-  }
+  backgroundStyle: {
+    flex: 1,
+  },
 });
